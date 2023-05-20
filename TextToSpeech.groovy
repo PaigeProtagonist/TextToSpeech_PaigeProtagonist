b3797d292f2a66b934dd872c6b24e19788a75261
bradk <bradk@LAPTOP-AD7JN7RD>
2023-05-20 14:15:10 -0400

    run full thing

--- TextToSpeech.groovy
+++ TextToSpeech.groovy
@@ -11,6 +11,7 @@ int chunkSize =500
 int numChunks = contents.size()/chunkSize
 
 for(int i=0;i<contents.size() && !Thread.interrupted();i+=chunkSize){
+
 	println "Chunk number "+i;
 	BowlerStudio.speak(contents.substring(i,i+500))
 	Thread.sleep(1)
