bd19095f3eeaf4b76e3ff143a6925b826500c8d0
bradk <bradk@LAPTOP-AD7JN7RD>
2023-05-20 13:51:46 -0400

    load text

--- TextToSpeech.groovy
+++ TextToSpeech.groovy
@@ -1,31 +1,7 @@
+String location ="C:\\Users\\bradk\\Documents\\gridlockkk.txt"
 
-BowlerKernel.speak("This is the default voice");
-BowlerKernel.speak("Voice one", 100, 0, 300, 1.0, 1.0);
-BowlerKernel.speak("Voice two", 100, 0, 200, 1.0, 1.0);
-BowlerKernel.speak("Voice three", 100, 0, 100, 1.0, 1.0);
-BowlerKernel.speak("Voice four", 100, 0, 50, 1.0, 1.0);
-BowlerKernel.speak(
-	"we are the borg; rsistnce is feutle",
-	100, //rate 
-	0, //slur
-	300,  //voice
-	1,   //echo
-	0.4 	//volume
-);
-BowlerKernel.speak(
-	"An echoing voice in a stadium",
-	300, //rate 
-	0, //slur
-	300,  //voice
-	0.5,   //echo
-	1 	//volume
-);
-////
-BowlerKernel.speak(
-		"The robot voice of brittish starscream",
-		100, //rate 
-		90, //slur
-		200,  //voice
-		1,   //echo
-		1 	//volume
-);
+File f = new File(location)
+
+String contents = f.text
+
+println contents
